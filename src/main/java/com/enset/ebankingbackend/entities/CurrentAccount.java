630d8b0a7612fb096c5b65d947f3dac39f1c8c82
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-05-24 19:07:13 +0000

    creation des class, Enum et repository

---
+++ src/main/java/com/enset/ebankingbackend/entities/CurrentAccount.java
@@ -0,0 +1,14 @@
+package entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("CC")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class CurrentAccount extends BankAccount{
+    private double overDraft;
+}
---
+++ src/main/java/com/enset/ebankingbackend/repositories/AccountOperationRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import entities.AccountOperation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountOperationRepository extends JpaRepository<AccountOperation, Long> {
+}
