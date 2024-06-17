b4d7449624a5dfc1b66746866d78465ba234a920
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-06-17 18:06:12 +0000

    auth inMemory

--- src/main/java/com/enset/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/com/enset/ebankingbackend/web/CustomerRestController.java
@@ -6,6 +6,7 @@ import com.enset.ebankingbackend.exceptions.CustomerNotFoundException;
 import com.enset.ebankingbackend.services.BankAccountService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,33 +18,40 @@ import java.util.List;
 public class CustomerRestController {
     private BankAccountService bankAccountService;
 
+
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomers();
     }
 
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> searchCustomers(@RequestParam(name="keyword", defaultValue = "") String keyword){
         return bankAccountService.searchCustomers("%"+keyword+"%");
     }
 
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public CustomerDTO customers(@PathVariable Long id) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(id);
     }
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO request){
         return bankAccountService.saveCustomer(request);
     }
 
     @PutMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO updateCustomer(@PathVariable Long id, @RequestBody CustomerDTO request){
         request.setId(id);
         return bankAccountService.updateCustomer(request);
     }
 
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable Long id){
         bankAccountService.deleteCustomer(id);
     }
