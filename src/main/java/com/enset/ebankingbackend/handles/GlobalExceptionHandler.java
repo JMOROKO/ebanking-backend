4409d81cd9545fce9da08009e1faa8b756ca606f
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-08-22 16:23:30 +0000

    completion des erreurs

--- src/main/java/com/enset/ebankingbackend/handles/GlobalExceptionHandler.java
+++ src/main/java/com/enset/ebankingbackend/handles/GlobalExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.enset.ebankingbackend.handles;
 
+import com.enset.ebankingbackend.exceptions.BalanceNotSufficentException;
+import com.enset.ebankingbackend.exceptions.BankAccountNotFoundException;
+import com.enset.ebankingbackend.exceptions.CustomerNotFoundException;
+import com.enset.ebankingbackend.exceptions.SameAccountException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
@@ -31,5 +36,41 @@ public class GlobalExceptionHandler {
                 .body(representation);
     }
 
-
+    @ExceptionHandler(BalanceNotSufficentException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(BalanceNotSufficentException exception) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(representation);
+    }
+    @ExceptionHandler(BankAccountNotFoundException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(BankAccountNotFoundException exception) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(representation);
+    }
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(CustomerNotFoundException exception) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(representation);
+    }
+    @ExceptionHandler(SameAccountException.class)
+    public ResponseEntity<ExceptionRepresentation> handleException(SameAccountException exception) {
+        ExceptionRepresentation representation = ExceptionRepresentation.builder()
+                .errorMessage(exception.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(representation);
+    }
 }
